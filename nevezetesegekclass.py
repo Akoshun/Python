b7042aa8af02ab79bbee389720b38368b4323ba8
Akoshun <72446104+Akoshun@users.noreply.github.com>
2022-03-30 21:58:11 +0200

    Fájl-ok
    :D

---
+++ nevezetesegekclass.py
@@ -0,0 +1,8 @@
+class Nevezetesegek:
+    def __init__(self,nev,varos,orszag,kontinens):
+        self.nev=str(nev)
+        self.varos=str(varos)
+        self.orszag=str(orszag)
+        self.kontinens=str(kontinens)
+             
+
