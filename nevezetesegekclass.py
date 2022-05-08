da45e070123850c1b323ee195ec4a00c735d2d5b
Akoshun <72446104+Akoshun@users.noreply.github.com>
2022-05-08 17:03:42 +0200

    alma
    alma

--- nevezetesegekclass.py
+++
@@ -1,8 +0,0 @@
-class Nevezetesegek:
-    def __init__(self,nev,varos,orszag,kontinens):
-        self.nev=str(nev)
-        self.varos=str(varos)
-        self.orszag=str(orszag)
-        self.kontinens=str(kontinens)
-             
-
